a8ce55f9fe37845b5becdb375f94c37167d8ff11
mk8bk <moncef.aitbelkacem@gmail.com>
2021-11-27 01:53:10 +0100

    12 sans tests et docu

---
+++ 12/2048.cpp
@@ -0,0 +1,19 @@
+#include "modele.h"
+#include <vector>
+#include <iostream>
+#include <cstdlib> 
+#include <time.h>
+using namespace std;
+typedef vector<vector<int>> Plateau;
+
+
+// * "g++ -c modele.cpp"
+// * "g++ -c 2048.cpp"
+// * "g++ 2048.o modele.o -o 2048"
+
+//jeu()
+
+int main(){
+
+	jeu();
+}
